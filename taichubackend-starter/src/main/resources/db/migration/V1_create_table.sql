f24743a771f8a601e0be61976140e0f33ae34c60
chai <roy.chai.whu@gmail.com>
2025-06-14 00:28:07 +0800

    update SQL

--- taichubackend-starter/src/main/resources/db/migration/V1_create_table.sql
+++ taichubackend-starter/src/main/resources/db/migration/V1_create_table.sql
@@ -1,114 +1,95 @@
--- 创建用户表 (fic_user)
-CREATE TABLE fic_user (
-                          id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '用户id',
-                          gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                          phone_number varchar(32) COMMENT '电话号码'
+create table fic_algo_task
+(
+    id               bigint auto_increment comment 'task id'
+        primary key,
+    gmt_create       bigint      not null comment '创建时间（毫秒时间戳）',
+    workflow_task_id bigint      not null comment 'workflow_task_id',
+    status           tinyint     not null comment '1-RUNNING 2-SUCCESS 0-FAIL',
+    task_type        varchar(64) not null comment 'SCRIPT_GENERATION, STORYBOARD_GENERATION, STORYBOARD_IMG_GENERATION, STORYBOARD_VIDEO_GENERATION, FULL_VIDEO_GENERATION',
+    algo_task_id     bigint      null comment '算法task id',
+    task_abstract    text        null comment '任务参数摘要',
+    relevant_id_type varchar(64) null comment 'workflowId / 分镜id',
+    relevant_id      bigint      null comment 'workflowId 或者 分镜id'
 );
 
--- 创建工作流表 (fic_workflow)
-CREATE TABLE fic_workflow (
-                              id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '工作流id',
-                              user_id bigint NOT NULL COMMENT '关联用户id',
-                              gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                              status tinyint NOT NULL COMMENT '状态',
-                              extend_info text COMMENT '扩展字段，jsonObject格式'
+create table fic_resource
+(
+    id                    bigint auto_increment comment 'id'
+        primary key,
+    gmt_create            bigint       not null,
+    workflow_id           bigint       not null comment 'id',
+    status                tinyint      not null comment '1- 0-',
+    relevance_id          bigint       not null,
+    relevance_type        varchar(32)  not null,
+    resource_type         varchar(32)  not null,
+    resource_storage_type varchar(32)  not null comment ': FILE_SYS / ALICLOUD_OSS / AMAZON_S3 ',
+    resource_url          varchar(256) not null,
+    extend_info           text         null comment 'jsonObject'
 );
 
--- 创建剧本表（fic_script）
-CREATE TABLE fic_script (
-                            id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '资源id',
-                            workflow_id bigint NOT NULL COMMENT '工作流id',
-                            gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                            status tinyint NOT NULL COMMENT '1-有效 0-无效',
-                            order_index bigint NOT NULL COMMENT '展示顺序，从0开始递增',
-                            content text COMMENT '剧本文本',
-                            extend_info text COMMENT '扩展字段，jsonObject格式'
+create table fic_role
+(
+    id          bigint auto_increment comment '资源id'
+        primary key,
+    workflow_id bigint       not null comment '工作流id',
+    gmt_create  bigint       not null comment '创建时间（毫秒时间戳）',
+    status      tinyint      not null comment '1-有效 0-无效',
+    role_name   varchar(128) not null comment '人物名称',
+    description text         null comment '角色初始形象描述',
+    prompt      text         null comment '角色描述，用于生成角色初始形象图',
+    extend_info text         null comment '扩展字段，jsonObject格式'
 );
 
--- 创建角色表（fic_role）
-CREATE TABLE fic_role (
-                          id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '资源id',
-                          workflow_id bigint NOT NULL COMMENT '工作流id',
-                          gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                          status tinyint NOT NULL COMMENT '1-有效 0-无效',
-                          role_name varchar(128) NOT NULL COMMENT '人物名称',
-                          description text COMMENT '角色初始形象描述',
-                          prompt text COMMENT '角色描述，用于生成角色初始形象图',
-                          extend_info text COMMENT '扩展字段，jsonObject格式'
+create table fic_script
+(
+    id          bigint auto_increment comment '资源id'
+        primary key,
+    workflow_id bigint  not null comment '工作流id',
+    gmt_create  bigint  not null comment '创建时间（毫秒时间戳）',
+    status      tinyint not null comment '1-有效 0-无效',
+    order_index bigint  not null comment '展示顺序，从0开始递增',
+    content     text    null comment '剧本文本',
+    extend_info text    null comment '扩展字段，jsonObject格式'
 );
 
-
--- 创建分镜表（fic_storyboard）
-CREATE TABLE fic_storyboard (
-                                id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '分镜id',
-                                workflow_id bigint NOT NULL COMMENT '工作流id',
-                                gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                                status tinyint NOT NULL COMMENT '1-有效 0-无效',
-                                script_id bigint NOT NULL COMMENT '关联的剧本分片id',
-                                order_index bigint NOT NULL COMMENT '展示顺序 关联的剧本分片展示顺序 * 10000 + 自身的展示顺序[1] 自身展示顺序从0开始递增',
-                                content text COMMENT '分镜描述',
-                                extend_info text COMMENT '扩展字段，jsonObject格式'
+create table fic_storyboard
+(
+    id          bigint auto_increment comment '分镜id'
+        primary key,
+    workflow_id bigint  not null comment '工作流id',
+    gmt_create  bigint  not null comment '创建时间（毫秒时间戳）',
+    status      tinyint not null comment '1-有效 0-无效',
+    script_id   bigint  not null comment '关联的剧本分片id',
+    order_index bigint  not null comment '展示顺序 关联的剧本分片展示顺序 * 10000 + 自身的展示顺序[1] 自身展示顺序从0开始递增',
+    content     text    null comment '分镜描述',
+    extend_info text    null comment '扩展字段，jsonObject格式'
 );
 
-# -- 创建分镜图片表
-# CREATE TABLE fic_resource (
-#                               id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '分镜图id',
-#                               gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-#                               workflow_id bigint NOT NULL COMMENT '工作流id',
-#                               storyboard_id bigint NOT NULL COMMENT '分镜id',
-#                               status tinyint NOT NULL COMMENT '1-有效 0-无效',
-#                               resource_storage_type varchar(32) NOT NULL COMMENT '存储方式: FILE_SYS / ALICLOUD_OSS / AMAZON_S3 等等，可扩展',
-#                               resource_url varchar(256) NOT NULL COMMENT '存储路径',
-#                               extend_info text COMMENT '扩展字段，jsonObject格式'
-# );
-#
-# -- 创建分镜视频表
-# CREATE TABLE fic_resource (
-#                               id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '分镜视频id',
-#                               gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-#                               workflow_id bigint NOT NULL COMMENT '工作流id',
-#                               storyboard_id bigint NOT NULL COMMENT '分镜id',
-#                               status tinyint NOT NULL COMMENT '1-有效 0-无效',
-#                               resource_storage_type varchar(32) NOT NULL COMMENT '存储方式: FILE_SYS / ALICLOUD_OSS / AMAZON_S3 等等，可扩展',
-#                               resource_url varchar(256) NOT NULL COMMENT '存储路径',
-#                               extend_info text COMMENT '扩展字段，jsonObject格式'
-# );
-#
-# -- 创建workflow合并视频表
-# CREATE TABLE fic_resource (
-#                               id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '合并视频id',
-#                               gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-#                               workflow_id bigint NOT NULL COMMENT '工作流id',
-#                               status tinyint NOT NULL COMMENT '1-有效 0-无效',
-#                               resource_storage_type varchar(32) NOT NULL COMMENT '存储方式: FILE_SYS / ALICLOUD_OSS / AMAZON_S3 等等，可扩展',
-#                               resource_url varchar(256) NOT NULL COMMENT '存储路径',
-#                               extend_info text COMMENT '扩展字段，jsonObject格式'
-# );
-
-
--- 创建资源表（fic_resource）
-CREATE TABLE fic_resource (
-                              id bigint PRIMARY KEY AUTO_INCREMENT COMMENT '分镜图id',
-                              gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                              workflow_id bigint NOT NULL COMMENT '工作流id',
-                              status tinyint NOT NULL COMMENT '1-有效 0-无效',
-                              relevance_id bigint NOT NULL COMMENT '关联键',
-                              relevance_type varchar(32) NOT NULL COMMENT '关联键类型',
-                              resource_type varchar(32) NOT NULL COMMENT '资源类型',
-                              resource_storage_type varchar(32) NOT NULL COMMENT '存储方式: FILE_SYS / ALICLOUD_OSS / AMAZON_S3 等等，可扩展',
-                              resource_url varchar(256) NOT NULL COMMENT '存储路径',
-                              extend_info text COMMENT '扩展字段，jsonObject格式'
+create table fic_user
+(
+    id           bigint auto_increment comment '用户id'
+        primary key,
+    gmt_create   bigint      not null comment '创建时间（毫秒时间戳）',
+    phone_number varchar(32) null comment '电话号码'
 );
 
--- 创建任务表（fic_task）
-CREATE TABLE fic_task (
-                          id bigint PRIMARY KEY AUTO_INCREMENT COMMENT 'task id',
-                          gmt_create bigint NOT NULL COMMENT '创建时间（毫秒时间戳）',
-                          workflow_id bigint NOT NULL COMMENT 'workflow_id',
-                          status tinyint NOT NULL COMMENT '1-RUNNING 2-SUCCESS 0-FAIL',
-                          task_type varchar(64) NOT NULL COMMENT 'SCRIPT_GENERATION, STORYBOARD_GENERATION, STORYBOARD_IMG_GENERATION, STORYBOARD_VIDEO_GENERATION, FULL_VIDEO_GENERATION',
-                          relevance_id bigint NOT NULL COMMENT '关联键',
-                          relevance_type varchar(32) NOT NULL COMMENT '关联键类型',
-                          algo_task_id bigint COMMENT '算法task id',
-                          task_abstract text COMMENT '任务参数摘要'
+create table fic_workflow
+(
+    id          bigint auto_increment comment '工作流id'
+        primary key,
+    user_id     bigint  not null comment '关联用户id',
+    gmt_create  bigint  not null comment '创建时间（毫秒时间戳）',
+    status      tinyint not null comment '状态',
+    extend_info text    null comment '扩展字段，jsonObject格式'
+);
+
+create table fic_workflow_task
+(
+    id          bigint auto_increment comment 'workflow task id'
+        primary key,
+    gmt_create  bigint        not null comment '创建时间（毫秒时间戳）',
+    workflow_id bigint        not null comment 'workflow_id',
+    status      tinyint       not null comment '1-RUNNING 2-SUCCESS 0-FAIL',
+    task_type   varchar(64)   not null comment 'SCRIPT_GENERATION, STORYBOARD_IMG_GENERATION, STORYBOARD_VIDEO_GENERATION,FULL_VIDEO_GENERATION, USER_RETRY_SINGLE_STORYBOARD_IMG_GENERATION, USER_RETRY_SINGLE_STORYBOARD_VIDEO_GENERATION, USER_RETRY_FULL_VIDEO_GENERATION',
+    params      varchar(1024) null comment '重要任务参数（json格式，everything is string）'
 );
