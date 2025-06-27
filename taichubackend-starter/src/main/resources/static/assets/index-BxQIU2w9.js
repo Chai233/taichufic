61af881c66c9823f261ac53e90d75540f3a6029f
chai <roy.chai.whu@gmail.com>
2025-06-27 21:32:51 +0800

    update static files

---
+++ taichubackend-starter/src/main/resources/static/assets/index-BxQIU2w9.js
@@ -0,0 +1,1 @@
+import{p as o}from"./index-BBlj73H0.js";const t=()=>o.jsx("div",{className:"common-protocol",children:"Copyright © 2024 Remotely All Rights Reserved. 北京印刷学院 | 沪ICP备2023021309号-1 "});export{t as P};
