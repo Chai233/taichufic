2d00684c64fe144df3036907150421c268840a21
chai <roy.chai.whu@gmail.com>
2025-12-04 22:56:40 +0800

    前端update

--- taichubackend-starter/src/main/resources/static/assets/index-lDxb9Z7l.js
+++
@@ -1,1 +0,0 @@
-import{p as o}from"./index-BTP23qkG.js";const t=()=>o.jsx("div",{className:"common-protocol",children:"Copyright © 2024 Remotely All Rights Reserved. 北京印刷学院 | 沪ICP备2023021309号-1 "});export{t as P};
